fd3695d77158da4bc1c17335a110142b6e485e66
natgarrod <nataliagarciarodriguez7@gmail.com>
2025-06-23 21:50:58 +0200

    Añadido un loader de reserva para probar

---
+++ src/main/java/com/atm/buenas_practicas_java/loaders/LoaderReservaPrueba.java
@@ -0,0 +1,274 @@
+package com.atm.buenas_practicas_java.loaders;
+
+import com.atm.buenas_practicas_java.entities.Habitacion;
+import com.atm.buenas_practicas_java.entities.Hotel;
+import com.atm.buenas_practicas_java.repositories.EntidadHijaRepository;
+import com.atm.buenas_practicas_java.repositories.EntidadPadreRepository;
+import com.atm.buenas_practicas_java.repositories.HabitacionRepo;
+import com.atm.buenas_practicas_java.repositories.HotelesRepo;
+import com.atm.buenas_practicas_java.entities.Reserva;
+import com.atm.buenas_practicas_java.repositories.ReservaRepo;
+import com.atm.buenas_practicas_java.entities.Usuario;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+
+
+/**
+ * Clase de configuración que se utiliza exclusivamente para el perfil "default" en entornos locales.
+ *
+ * Su propósito principal es cargar datos de ejemplo en las bases de datos asociadas a las entidades
+ * {@code EntidadPadre} y {@code EntidadHija}, permitiendo la inicialización de datos útiles para pruebas
+ * y desarrollo en este perfil específico.
+ *
+ * Esta clase está anotada con:
+ * - {@code @Configuration}: Define esta clase como fuente de beans y configuración.
+ * - {@code @Log4j2}: Habilita el uso de la biblioteca Log4j2 para registro de mensajes en los logs.
+ * - {@code @Profile("default")}: Asegura que esta clase solo se cargue en el perfil "default".
+ *
+ * @see EntidadPadreRepository
+ * @see EntidadHijaRepository
+ */
+@Configuration
+@Log4j2
+@Profile("local")
+public class LoaderReservaPrueba {
+    /*public class LocalDataLoader {*/
+    private final HotelesRepo hotelesRepo;
+    private final HabitacionRepo habitacionRepo;
+    private final ReservaRepo reservaRepo;
+
+    /**
+     * Constructor de la clase {@code LocalDataLoader}.
+     * <p>
+     * Inicializa un objeto {@code LocalDataLoader} configurado con los repositorios de las entidades,
+     * proporcionando la capacidad de interactuar con estas entidades en la base de datos.
+     */
+    /* public LocalDataLoader(HotelesRepo  hotelesRepo,  HabitacionRepo habitacionRepo) {*/
+    public LoaderReservaPrueba(HotelesRepo hotelesRepo, HabitacionRepo habitacionRepo, ReservaRepo reservaRepo) {
+        this.hotelesRepo = hotelesRepo;
+        this.habitacionRepo = habitacionRepo;
+        this.reservaRepo = reservaRepo;
+    }
+
+    /**
+     * Método anotado con {@code @PostConstruct} que inicializa datos de prueba en
+     * los repositorios para entornos locales. Este método se ejecuta automáticamente
+     * después de la inicialización del bean y antes de que esté disponible para uso,
+     * permitiendo cargar datos iniciales necesarios para el perfil local.
+     * <p>
+     * Funcionalidad:
+     * - Crea 10 instancias de la entidad `EntidadPadre` con nombres predefinidos.
+     * - Guarda las instancias de `EntidadPadre` en el repositorio correspondiente.
+     * - Para cada instancia de `EntidadPadre`, crea una entidad relacionada de tipo
+     * `EntidadHija` con un nombre identificativo, y la asocia a la entidad padre
+     * pertinente.
+     * - Guarda las entidades hijas en el repositorio `entidadHijaRepository`.
+     * - Registra mensajes informativos en el log sobre el inicio y finalización del proceso.
+     * <p>
+     * Proceso:
+     * 1. Se define un número fijo de entidades padre (10).
+     * 2. Se utiliza un array para almacenar las instancias y se inicializa con un nombre
+     * único para cada entidad padre.
+     * 3. Todas las entidades padre se guardan de forma simultánea utilizando
+     * {@code repository.saveAll}.
+     * 4. Para cada entidad padre, se crea una instancia de la entidad hija, se establece
+     * la relación con el padre y se guarda en el repositorio correspondiente.
+     * 5. Se registran logs informativos sobre el estado del proceso.
+     * <p>
+     * Dependencias principales:
+     * - `repository`: {@code EntidadPadreRepository}, usado para almacenar las entidades padre.
+     * - `entidadHijaRepository`: {@code EntidadHijaRepository}, usado para guardar las entidades hijas.
+     * <p>
+     * Importante:
+     * - Este método está diseñado específicamente para ser utilizado en entornos con
+     * el perfil local activo.
+     * - No debe usarse en entornos de producción, ya que sobrescribirá datos existentes.
+     * <p>
+     * Logs:
+     * - Mensaje al inicio del proceso: "Iniciando la carga de datos para el perfil local".
+     * - Mensaje exitoso al finalizar: "Datos de entidades cargados correctamente."
+     */
+    @PostConstruct
+    public void loadDataDesarrollo() {
+        loadHoteles();
+        loadReservas();
+    }
+
+    public void loadHoteles() {
+        log.info("Iniciando carga de hoteles ficticios...");
+
+        SaveAllHoteles();
+        SaveAllHabitaciones();
+
+        log.info("Carga completada");
+    }
+
+    private Hotel hotel1;
+    private Hotel hotel2;
+    private Hotel hotel3;
+
+    private void SaveAllHabitaciones() {
+        List<Habitacion> habitaciones = new ArrayList<>();
+
+        Habitacion habitacion = habitacion1();
+        Habitacion habitacion2 = habitacion2();
+        Habitacion habitacion3 = habitacion3();
+        habitaciones.add(habitacion);
+        habitaciones.add(habitacion2);
+        habitaciones.add(habitacion3);
+
+        habitacionRepo.saveAll(habitaciones);
+    }
+
+    private void SaveAllHoteles() {
+        List<Hotel> hoteles = new ArrayList<>();
+
+        hotel1 = getHotel();
+        hoteles.add(hotel1);
+
+        hotel2 = getHotel2();
+        hoteles.add(hotel2);
+
+        hotel3 = getHotel3();
+        hoteles.add(hotel3);
+
+        hotelesRepo.saveAll(hoteles);
+    }
+
+    private Hotel getHotel3() {
+        Hotel hotel3 = new Hotel();
+        hotel3.setNombre("Costa Azul Resort");
+        hotel3.setDescripcion("Relájate frente al mar Mediterráneo");
+        hotel3.setCiudad("Alicante");
+        hotel3.setDireccion("Avenida del Mar, 55, 03001 Alicante");
+        hotel3.setDireccionURL("https://maps.google.com/?q=Avenida+del+Mar+55+Alicante");
+        hotel3.setImageURL("https://picsum.photos/600/400?random=12");
+        hotel3.setTelefono("+34 611 555 666");
+        hotel3.setEmail("contacto@costazulresort.com");
+        return hotel3;
+    }
+
+    private Hotel getHotel2() {
+        Hotel hotel2 = new Hotel();
+        hotel2.setNombre("Monteverde Palace");
+        hotel2.setDescripcion("Lujo entre montañas y aire puro");
+        hotel2.setCiudad("Granada");
+        hotel2.setDireccion("Camino de la Sierra, s/n, 18010, Granada");
+        hotel2.setDireccionURL("https://maps.google.com/?q=Camino+de+la+Sierra+Granada");
+        hotel2.setImageURL("https://picsum.photos/600/400?random=8");
+        hotel2.setTelefono("+34 622 333 444");
+        hotel2.setEmail("reservas@monteverdepalace.com");
+        return hotel2;
+    }
+
+    private Hotel getHotel() {
+        Hotel hotel1 = new Hotel();
+        hotel1.setNombre("Tresora Beach");
+        hotel1.setDescripcion("Un paraíso costero para que te lo goces");
+        hotel1.setCiudad("Málaga");
+        hotel1.setDireccion("Playa de Pedregalejo, Málaga-Este, 29017, Málaga");
+        hotel1.setDireccionURL("https://maps.google.com/?q=Playa+de+Pedregalejo");
+        hotel1.setImageURL("https://picsum.photos/600/400?random=5");
+        hotel1.setTelefono("+34 633 111 222");
+        hotel1.setEmail("info@tresorabeach.com");
+        return hotel1;
+    }
+
+    private Habitacion habitacion1() {
+        Habitacion habitacion1 = new Habitacion();
+        habitacion1.setIdHotel(hotel1);
+        habitacion1.setIdProducto(null);
+        habitacion1.setNumeroHabitacion(0);
+        habitacion1.setPiso(0);
+        habitacion1.setTipo("Individual");
+        habitacion1.setCapacidad(1);
+        habitacion1.setEstadoOcupacion("Libre");
+        return habitacion1;
+    }
+
+    private Habitacion habitacion2() {
+        Habitacion habitacion2 = new Habitacion();
+        habitacion2.setIdHotel(hotel2);
+        habitacion2.setIdProducto(null);
+        habitacion2.setNumeroHabitacion(101);
+        habitacion2.setPiso(1);
+        habitacion2.setTipo("Doble");
+        habitacion2.setCapacidad(2);
+        habitacion2.setEstadoOcupacion("Ocupado");
+        return habitacion2;
+    }
+
+    private Habitacion habitacion3() {
+        Habitacion habitacion3 = new Habitacion();
+        habitacion3.setIdHotel(hotel3);
+        habitacion3.setIdProducto(null);
+        habitacion3.setNumeroHabitacion(202);
+        habitacion3.setPiso(2);
+        habitacion3.setTipo("Suite");
+        habitacion3.setCapacidad(4);
+        habitacion3.setEstadoOcupacion("Libre");
+        return habitacion3;
+    }
+
+
+    public void loadReservas() {
+        log.info("Loading reservas ficticias...");
+        SaveAllReservas();
+
+        log.info("Reservas ficticias loaded");
+    }
+
+    private void SaveAllReservas() {
+        List<Reserva> reservas = new ArrayList<>();
+
+        Reserva reserva1 = getReserva1();
+        Reserva reserva2 = getReserva2();
+
+        reservas.add(reserva1);
+        reservas.add(reserva2);
+
+        reservaRepo.save(reserva1);
+
+    }
+
+    private Reserva getReserva1() {
+        Reserva reserva1 = new Reserva();
+        reserva1.setIdUsuario(createUsuarioFicticio(999999, "UserPrueba"));
+        reserva1.setIdHabitacion(habitacion1());
+        reserva1.setFechaEntrada(LocalDate.of(2025, 7, 27));
+        reserva1.setFechaSalida(LocalDate.of(2025, 7, 29));
+        reserva1.setEstado("Confirmada");
+        reserva1.setPax(2);
+        reserva1.setFechaReserva(Instant.now());
+        reserva1.setComentarios("");
+        return reserva1;
+    }
+
+    private Reserva getReserva2() {
+        Reserva reserva2 = new Reserva();
+        reserva2.setIdUsuario(createUsuarioFicticio(999999, "UserPrueba"));
+        reserva2.setIdHabitacion(habitacion2());
+        reserva2.setFechaEntrada(LocalDate.of(2025, 8, 1));
+        reserva2.setFechaSalida(LocalDate.of(2025, 8, 10));
+        reserva2.setEstado("Pendiente");
+        reserva2.setPax(3);
+        reserva2.setFechaReserva(Instant.now());
+        reserva2.setComentarios("Alergia almendras");
+        return reserva2;
+    }
+
+    private Usuario createUsuarioFicticio(Integer id, String nombre) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre(nombre);
+        return usuario;
+    }
+}
