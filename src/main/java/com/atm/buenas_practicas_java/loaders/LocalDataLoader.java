889a33d7d53432dd0d966ea54e7f0c640e044dbc
alvarosevilla96 <alvarof.sev.96@gmail.com>
2025-06-23 18:50:01 +0200

    Prueba de data loader rol

--- src/main/java/com/atm/buenas_practicas_java/loaders/LocalDataLoader.java
+++ src/main/java/com/atm/buenas_practicas_java/loaders/LocalDataLoader.java
@@ -37,16 +37,19 @@ public class LocalDataLoader {
     private final HotelesRepo  hotelesRepo;
     private final HabitacionRepo habitacionRepo;
     private final UsuarioRepo usuarioRepo;
+    private final RolRepo rolRepo;
     /**
      * Constructor de la clase {@code LocalDataLoader}.
      *
      * Inicializa un objeto {@code LocalDataLoader} configurado con los repositorios de las entidades,
      * proporcionando la capacidad de interactuar con estas entidades en la base de datos.
      */
-    public LocalDataLoader(HotelesRepo  hotelesRepo, HabitacionRepo habitacionRepo, UsuarioRepo usuarioRepo) {
+    public LocalDataLoader(HotelesRepo  hotelesRepo, HabitacionRepo habitacionRepo, UsuarioRepo usuarioRepo
+                            , RolRepo rolRepo) {
         this.hotelesRepo = hotelesRepo;
         this.habitacionRepo = habitacionRepo;
         this.usuarioRepo = usuarioRepo;
+        this.rolRepo = rolRepo;
     }
 
     /**
@@ -90,6 +93,7 @@ public class LocalDataLoader {
     @PostConstruct
     public void loadDataDesarrollo() {
         loadHoteles();
+        loadRoles();
         loadUsuarios();
     }
 
@@ -102,19 +106,51 @@ public class LocalDataLoader {
         log.info("Carga completada");
     }
 
+    public void loadRoles() {
+        log.info("Iniciando carga de roles ficticios...");
+        if(!rolRepo.existsById(1)) {
+            Rol cliente = new Rol();
+            cliente.setId(1);
+            cliente.setNombreRol("cliente");
+            rolRepo.save(cliente);
+        }
+        if(!rolRepo.existsById(2)) {
+            Rol empleado = new Rol();
+            empleado.setId(2);
+            empleado.setNombreRol("empleado");
+            rolRepo.save(empleado);
+        }
+        if(!rolRepo.existsById(3)) {
+            Rol limpieza = new Rol();
+            limpieza.setId(3);
+            limpieza.setNombreRol("limpieza");
+            rolRepo.save(limpieza);
+        }
+        if(!rolRepo.existsById(4)) {
+            Rol admin = new Rol();
+            admin.setId(4);
+            admin.setNombreRol("admin");
+            rolRepo.save(admin);
+        }
+    }
+
     public void loadUsuarios() {
         log.info("Iniciando carga de usuarios ficticios...");
+        Rol rolCliente = rolRepo.findById(1).orElseThrow();
+        Rol rolEmpleado = rolRepo.findById(2).orElseThrow();
+        Rol rolLimpieza = rolRepo.findById(3).orElseThrow();
+        Rol rolAdmin = rolRepo.findById(4).orElseThrow();
         List<Usuario> usuarios = new ArrayList<>();
-        usuarios.add(crearUsuario(null, null, "Lucas", "Martínez", "lucas.martinez@mail.com", "pass1234", "Calle A, Madrid", "612345678", LocalDate.of(1985, 7, 12), LocalDate.of(2023, 5, 1), true, "12345678A"));
-        usuarios.add(crearUsuario(null, null, "Sophie", "Dupont", "sophie.dupont@mail.fr", "bonjour2023", "5 Rue Rivoli, Paris", "3312345678", LocalDate.of(1992, 3, 22), LocalDate.of(2024, 1, 15), true, "FR9876543"));
-        usuarios.add(crearUsuario(null, null, "Ahmed", "El-Sayed", "ahmed.sayed@mail.com", "egypt321", "Cairo Road 3, Cairo", "201234567890", LocalDate.of(1990, 12, 1), LocalDate.of(2023, 11, 2), true, "EGP223344"));
-        usuarios.add(crearUsuario(null, hotel1, "Marta", "Gómez", "marta.gomez@hotelciudad.com", "empleado1", "Hotel Ciudad, Madrid", "613456789", LocalDate.of(1988, 6, 5), LocalDate.of(2022, 3, 20), true, "78965432Z"));
-        usuarios.add(crearUsuario(null, hotel2, "Carlos", "Ruiz", "carlos.ruiz@hotelcampo.com", "empleado2", "Hotel Campo, Sevilla", "611234567", LocalDate.of(1991, 2, 10), LocalDate.of(2022, 6, 15), true, "15975362Y"));
-        usuarios.add(crearUsuario(null, hotel1, "Lola", "Fernández", "lola.fernandez@limpieza.com", "limpieza1", "Hotel Ciudad, Piso 1", "622334455", LocalDate.of(1978, 11, 11), LocalDate.of(2021, 1, 10), true, "ESL123456"));
-        usuarios.add(crearUsuario(null, hotel2, "Ana", "Torres", "ana.torres@limpieza.com", "limpieza2", "Hotel Campo, Piso 2", "623456789", LocalDate.of(1982, 4, 19), LocalDate.of(2022, 4, 1), true, "ESL654321"));
-        usuarios.add(crearUsuario(null, hotel3, "Mateo", "Reyes", "mateo.reyes@limpieza.com", "limpieza3", "Hotel Playa, Piso 3", "624567890", LocalDate.of(1990, 1, 5), LocalDate.of(2023, 9, 20), true, "DNI998877"));
-        usuarios.add(crearUsuario(null, null, "Admin", "Principal", "admin@hoteles.com", "adminroot", "Oficina Central", "600000001", LocalDate.of(1975, 1, 1), LocalDate.of(2020, 1, 1), true, "ADM0001"));
-        usuarios.add(crearUsuario(null, null, "Emma", "Lopez", "emma.lopez@mail.com", "emmalopez", "Calle Falsa 123, Zaragoza", "611112222", LocalDate.of(1995, 8, 23), LocalDate.of(2024, 4, 3), true, "23456789L"));
+        usuarios.add(crearUsuario(rolCliente, null, "Lucas", "Martínez", "lucas.martinez@mail.com", "pass1234", "Calle A, Madrid", "612345678", LocalDate.of(1985, 7, 12), LocalDate.of(2023, 5, 1), true, "12345678A"));
+        usuarios.add(crearUsuario(rolCliente, null, "Sophie", "Dupont", "sophie.dupont@mail.fr", "bonjour2023", "5 Rue Rivoli, Paris", "3312345678", LocalDate.of(1992, 3, 22), LocalDate.of(2024, 1, 15), true, "FR9876543"));
+        usuarios.add(crearUsuario(rolCliente, null, "Ahmed", "El-Sayed", "ahmed.sayed@mail.com", "egypt321", "Cairo Road 3, Cairo", "201234567890", LocalDate.of(1990, 12, 1), LocalDate.of(2023, 11, 2), true, "EGP223344"));
+        usuarios.add(crearUsuario(rolEmpleado, hotel1, "Marta", "Gómez", "marta.gomez@hotelciudad.com", "empleado1", "Hotel Ciudad, Madrid", "613456789", LocalDate.of(1988, 6, 5), LocalDate.of(2022, 3, 20), true, "78965432Z"));
+        usuarios.add(crearUsuario(rolEmpleado, hotel2, "Carlos", "Ruiz", "carlos.ruiz@hotelcampo.com", "empleado2", "Hotel Campo, Sevilla", "611234567", LocalDate.of(1991, 2, 10), LocalDate.of(2022, 6, 15), true, "15975362Y"));
+        usuarios.add(crearUsuario(rolLimpieza, hotel1, "Lola", "Fernández", "lola.fernandez@limpieza.com", "limpieza1", "Hotel Ciudad, Piso 1", "622334455", LocalDate.of(1978, 11, 11), LocalDate.of(2021, 1, 10), true, "ESL123456"));
+        usuarios.add(crearUsuario(rolLimpieza, hotel2, "Ana", "Torres", "ana.torres@limpieza.com", "limpieza2", "Hotel Campo, Piso 2", "623456789", LocalDate.of(1982, 4, 19), LocalDate.of(2022, 4, 1), true, "ESL654321"));
+        usuarios.add(crearUsuario(rolLimpieza, hotel3, "Mateo", "Reyes", "mateo.reyes@limpieza.com", "limpieza3", "Hotel Playa, Piso 3", "624567890", LocalDate.of(1990, 1, 5), LocalDate.of(2023, 9, 20), true, "DNI998877"));
+        usuarios.add(crearUsuario(rolAdmin, null, "Admin", "Principal", "admin@hoteles.com", "adminroot", "Oficina Central", "600000001", LocalDate.of(1975, 1, 1), LocalDate.of(2020, 1, 1), true, "ADM0001"));
+        usuarios.add(crearUsuario(rolCliente, null, "Emma", "Lopez", "emma.lopez@mail.com", "emmalopez", "Calle Falsa 123, Zaragoza", "611112222", LocalDate.of(1995, 8, 23), LocalDate.of(2024, 4, 3), true, "23456789L"));
 
         usuarioRepo.saveAll(usuarios);
         log.info("Usuarios cargados: {}", usuarios.size());
@@ -227,11 +263,11 @@ public class LocalDataLoader {
         return habitacion3;
     }
 
-    private Usuario crearUsuario(Rol idRol, Hotel idHotel, String nombre, String apellidos,
+    private Usuario crearUsuario(Rol rol, Hotel idHotel, String nombre, String apellidos,
                                  String email, String password, String direccion, String telefono,
                                  LocalDate fechaNacimiento, LocalDate fechaAlta, boolean activo, String dni){
         Usuario usuario = new Usuario();
-        usuario.setIdRol(idRol);
+        usuario.setIdRol(rol);
         usuario.setIdHotel(idHotel);
         usuario.setNombre(nombre);
         usuario.setApellidos(apellidos);
