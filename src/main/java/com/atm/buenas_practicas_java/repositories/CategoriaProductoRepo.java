26838adeeb3a238bfdf12499e2cdee2035826983
natgarrod <nataliagarciarodriguez7@gmail.com>
2025-06-24 16:32:59 +0200

    cambios desde la rama de Jose

--- src/main/java/com/atm/buenas_practicas_java/repositories/CategoriaProductoRepo.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/CategoriaProductoRepo.java
@@ -1,9 +1,9 @@
 package com.atm.buenas_practicas_java.repositories;
 
-import com.atm.buenas_practicas_java.entities.Usuario;
+import com.atm.buenas_practicas_java.entities.CategoriaProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepo extends JpaRepository<Usuario, Integer> {
+public interface CategoriaProductoRepo extends JpaRepository<CategoriaProducto, Integer> {
 }
--- src/main/java/com/atm/buenas_practicas_java/repositories/ProductoRepo.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/ProductoRepo.java
@@ -1,9 +1,9 @@
 package com.atm.buenas_practicas_java.repositories;
 
-import com.atm.buenas_practicas_java.entities.Rol;
+import com.atm.buenas_practicas_java.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RolRepo extends JpaRepository<Rol, Integer> {
+public interface ProductoRepo extends JpaRepository<Producto, Integer> {
 }
