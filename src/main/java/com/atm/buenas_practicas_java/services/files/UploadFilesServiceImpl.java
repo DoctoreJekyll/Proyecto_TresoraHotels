d10beecd4c10e07ad165656283dda13740544bb9
Joseajierro <jrmar0805@gmail.com>
2025-07-04 18:56:16 +0200

    Upload files test, not working yet

---
+++ src/main/java/com/atm/buenas_practicas_java/services/files/UploadFilesServiceImpl.java
@@ -0,0 +1,54 @@
+package com.atm.buenas_practicas_java.services.files;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+
+public class UploadFilesServiceImpl implements IUploadFilesService {
+    @Override
+    public String handleFileUpload(MultipartFile file) {
+        try{
+            String fileName = UUID.randomUUID().toString()+"."+file.getOriginalFilename();
+            byte[] bytes = file.getBytes();
+            String fileOriginalName = file.getOriginalFilename();
+
+            long size = file.getSize();
+            long maxFileSize = 5 * 1024 * 1024;
+
+            if(size > maxFileSize){
+                return  "Upload File Too Large, must be less or equal to " + maxFileSize;
+            }
+
+            assert fileOriginalName != null;
+            if(!fileOriginalName.endsWith(".jpg") && !fileOriginalName.endsWith(".jpeg") &&  !fileOriginalName.endsWith(".png"))
+            {
+                return "Only jpg, jpeg or png files are supported";
+            }
+
+            String fileExtensionName = fileOriginalName.substring(fileOriginalName.lastIndexOf("."));
+            String newFileName = fileName + fileExtensionName;
+
+            File folder = new File("src/main/resources/pictures");
+            if(!folder.exists()){
+                folder.mkdir();
+            }
+
+            Path path = Paths.get(folder.getPath() + "/" + newFileName);
+            Files.write(path, bytes);
+
+            return "File uploaded successfully";
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
