85528bcfadf073c64f34bf944611d49a2f2624f9
AlbaGutierrezGarcia <alba2gr@gmail.com>
2025-07-17 22:08:46 +0200

    * feat: implementación de contacto para usuarios logueados * refactor: abstraer lógica de autenticación desde controlador al servicio (en contacto y en reserva)

--- src/main/java/com/atm/buenas_practicas_java/services/ContactoService.java
+++ src/main/java/com/atm/buenas_practicas_java/services/ContactoService.java
@@ -1,11 +1,33 @@
 package com.atm.buenas_practicas_java.services;
 
 import com.atm.buenas_practicas_java.entities.Contacto;
+import com.atm.buenas_practicas_java.entities.Usuario;
 import com.atm.buenas_practicas_java.repositories.ContactoRepo;
 import com.atm.buenas_practicas_java.services.templateMethod.AbstractTemplateServicesEntities;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactoService extends AbstractTemplateServicesEntities <Contacto,Integer, ContactoRepo> {
     public ContactoService(ContactoRepo repo) { super(repo); }
+
+
+    public Contacto contactoUsuarioLog(UsuarioService usuarioService) {
+        Contacto contacto = new Contacto();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()){
+            UserDetails user = (UserDetails) authentication.getPrincipal();
+            String userEmail = user.getUsername();
+
+            Usuario usuario = usuarioService.findByEmail(userEmail).orElseThrow();
+
+            contacto.setNombre(usuario.getNombre());
+            contacto.setCorreo(userEmail);
+        }
+        return contacto;
+    }
+
 }
