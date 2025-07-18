b6ee6e86c9a6536471c44f1f2f21c4469b5ea11e
natgarrod <nataliagarciarodriguez7@gmail.com>
2025-07-18 16:42:27 +0200

    controlador actualizado alvaro a natalia

--- src/main/java/com/atm/buenas_practicas_java/controllers/HotelesController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/HotelesController.java
@@ -134,17 +134,9 @@ public class HotelesController {
     @GetMapping("/login")
     public String mostrarLogin(Model model){
         LoginDto loginDto = new LoginDto();
-        loginDto.setEmail("test@ejemplo.com");
         model.addAttribute("datoslogin", loginDto);
         return "login";
     }
-
-    @PostMapping("/login")
-    public String postLogin(@ModelAttribute(name="datoslogin") LoginDto login){
-        System.out.println(login.getEmail());
-        return "user_home_page";
-    }
-
 
     @GetMapping("/crearCuenta")
     public String mostrarCrearCuenta(Model model ){
@@ -153,104 +145,11 @@ public class HotelesController {
         return "crearCuenta";
     }
 
-    private final EmailService emailService;
-
-    @Autowired
-    public HotelesController(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
     @PostMapping("/crearCuenta")
     public String postCrearCuenta(@ModelAttribute(name = "userData") UserDto user){
-        System.out.println(user.getName());
-        System.out.println(user.getEmail());
-        System.out.println(user.getPassword());
-
-        emailService.sendEmail(
-                "jrmar0805@gmail.com",
-                user.getEmail(),
-                "Creacion de cuenta en Tresora",
-                "Has creado tu cuenta con exito, estas son tus credenciales de usuario" + user.getEmail() + user.getPassword()
-        );
-
-        System.out.println("email sent");
-
         return "home";
     }
 
-    @GetMapping("/userhome")
-    public String mostrarUserHome( ) {
-        return "user_home_page";
-    }
 
-    @GetMapping("/contact")
-    public String mostrarPaginaContact(ModelMap intefrazConPantalla) {
-        ContactDto contacto = new ContactDto();
-        contacto.setName("El lobo feroz");
-        intefrazConPantalla.addAttribute("contactInfo", contacto);
-        return "contact";
-    }
-
-    @PostMapping("/contact")
-    public String postMostrarPaginaContact(@ModelAttribute(name="contactInfo") ContactDto contactInfo) {
-        System.out.println(contactInfo.getName());
-        System.out.println(contactInfo.getMessage());
-
-        emailService.sendEmail(
-
-                "alba2gr@gmail.com",
-                contactInfo.getEmail(),
-                "Gracias por contactar con Tresora",
-                "Hola " + contactInfo.getName() + ",\n\n" +
-                        "Hemos recibido tu mensaje:\n" +
-                        contactInfo.getMessage() + "\n\n" +
-                        "Te contactaremos pronto.\n\n" +
-                        "Atentamente,\nTresora Hotels"
-        );
-        return "home";
-    }
-
-    @GetMapping("/reservaPlaya")
-    public String reservaPlaya( ModelMap ModelReserva) {
-        ReservaDto reserva= new ReservaDto();
-        reserva.setAdultos(4);
-        ModelReserva.addAttribute("datosreserva", reserva);
-        return "reservaHotelPlaya";
-    }
-
-    @PostMapping("/reservaPlaya")
-    public String postReservaPlaya(@ModelAttribute(name="datosreserva") ReservaDto reserva) {
-        System.out.println(reserva.getAdultos());
-        System.out.println(reserva.getFechaEntrada());
-        System.out.println(reserva.getFechaSalida());
-        return "home";
-    }
-
-    @GetMapping("/reservarapida")
-    public String reservarapida( ) {
-        return "reservaRapida";
-    }
-
-    @GetMapping("/confirmarReserva")
-    public String confirmarreserva( ) {
-        return "confirmarReserva";
-    }
-
-
-    @GetMapping("/reservaMontaña")
-    public String reservaMontaña( ModelMap ModelReserva) {
-        ReservaDto reserva= new ReservaDto();
-        reserva.setAdultos(4);
-        ModelReserva.addAttribute("datosreserva", reserva);
-        return "reservaHotelMontaña";
-    }
-
-    @PostMapping("/reservaMontaña")
-    public String postReservaMontaña(@ModelAttribute(name="datosreserva") ReservaDto reserva) {
-        System.out.println(reserva.getAdultos());
-        System.out.println(reserva.getFechaEntrada());
-        System.out.println(reserva.getFechaSalida());
-        return "home";
-    }
 
 }
