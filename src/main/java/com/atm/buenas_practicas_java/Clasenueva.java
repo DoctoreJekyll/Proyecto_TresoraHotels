fd862dc211cda5c36844513cb8048c9926e458ae
Joseajierro <jrmar0805@gmail.com>
2025-07-21 10:34:47 +0200

    test to org

--- src/main/java/com/atm/buenas_practicas_java/Clasenueva.java
+++ src/main/java/com/atm/buenas_practicas_java/Clasenueva.java
@@ -7,5 +7,15 @@ public class Clasenueva {
         System.out.println("test");
     }
 
+    private void test3()
+    {
+        System.out.println("test3");
+    }
+
+    private void test4()
+    {
+        System.out.println("test4");
+    }
+
 
 }
