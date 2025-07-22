69b37e638ac5fc069a92b5dedc8489fd1330157f
Joseajierro <jrmar0805@gmail.com>
2025-07-22 19:03:41 +0200

    Revert "borrar entidades"
    This reverts commit e8b4d18dd2599a2d7d1e2be846ae7589f7aff260.

--- src/main/java/com/atm/buenas_practicas_java/entities/ProductoHabitacionId.java
+++ src/main/java/com/atm/buenas_practicas_java/entities/ProductoHabitacionId.java
@@ -1,37 +1,37 @@
-//package com.atm.buenas_practicas_java.entities;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Embeddable;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.hibernate.Hibernate;
-//
-//import java.io.Serializable;
-//import java.util.Objects;
-//
-//@Getter
-//@Setter
-//@Embeddable
-//public class ProductoHabitacionId implements Serializable {
-//
-//    @Column(name = "id_producto", nullable = false)
-//    private Integer idProducto;
-//
-//    @Column(name = "id_habitacion", nullable = false)
-//    private Integer idHabitacion;
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-//        ProductoHabitacionId entity = (ProductoHabitacionId) o;
-//        return Objects.equals(this.idProducto, entity.idProducto) &&
-//                Objects.equals(this.idHabitacion, entity.idHabitacion);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(idProducto, idHabitacion);
-//    }
-//
-//}
+package com.atm.buenas_practicas_java.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Embeddable
+public class ProductoHabitacionId implements Serializable {
+
+    @Column(name = "id_producto", nullable = false)
+    private Integer idProducto;
+
+    @Column(name = "id_habitacion", nullable = false)
+    private Integer idHabitacion;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ProductoHabitacionId entity = (ProductoHabitacionId) o;
+        return Objects.equals(this.idProducto, entity.idProducto) &&
+                Objects.equals(this.idHabitacion, entity.idHabitacion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProducto, idHabitacion);
+    }
+
+}
