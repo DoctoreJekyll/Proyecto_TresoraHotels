67b592f29ad9fd6ec791ba0a173018afb579f51f
Joseajierro <jrmar0805@gmail.com>
2025-07-24 00:10:05 +0200

    Fix reserva rapida sec

--- src/main/java/com/atm/buenas_practicas_java/config/SecurityConfig.java
+++ src/main/java/com/atm/buenas_practicas_java/config/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.security.web.SecurityFilterChain;
  * @Configuration Marca esta clase como una clase de configuración.
  * @EnableWebSecurity Habilita la seguridad web de Spring Security en la aplicación.
  *
- * @Author No se especificó autor.
  */
 @Configuration
 @EnableWebSecurity
@@ -40,34 +39,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-    /**
-     * Método que configura un {@link UserDetailsService} para la autenticación en memoria.
-     * Este método crea un usuario en memoria utilizando un nombre de usuario y contraseña
-     * definidos en las propiedades de configuración `spring.security.user.name` y
-     * `spring.security.user.password`, o valores predeterminados si no están configurados.
-     *
-     * <p>Nota: Se utiliza el identificador `{noop}` en la contraseña para evitar el uso
-     * de un encoder, ideal solo para pruebas. No debe ser utilizado en un entorno de producción.</p>
-     *
-     * @return Un {@link InMemoryUserDetailsManager} que contiene los detalles del usuario configurado.
-     *
-     * @Author No se especificó autor.
-     */
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        String name = environment.getProperty("spring.security.user.name", "user@hoteles.com");
-//        String password = environment.getProperty("spring.security.user.password", "password");
-//
-//        var user = User.withUsername(name)
-//                .password("{noop}" + password) // {noop} indica que no se usa encoder para simplificar (solo pruebas)
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-
     /**
      * Configura una cadena de filtros de seguridad para gestionar la seguridad HTTP de la aplicación.
      * Permite personalizar los comportamientos de seguridad como protección CSRF, autenticación básica,
@@ -87,8 +58,6 @@ public class SecurityConfig {
      * @return Un objeto {@link SecurityFilterChain} que representa la configuración de seguridad
      *         HTTP personalizada.
      * @throws Exception En caso de que ocurra algún error durante la configuración.
-     *
-     * @Author No se especificó autor.
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationSuccessHandler successHandler) throws Exception {
@@ -117,6 +86,7 @@ public class SecurityConfig {
                         .requestMatchers("/reserva/rapida").permitAll()
                         .requestMatchers("/contactos/nuevo").permitAll()
                         .requestMatchers("/hoteles").permitAll()
+                        .requestMatchers("/reserva/api/**").permitAll()
 
                         // Rutas específicas por rol
                         .requestMatchers("/panel").hasAnyRole("ADMIN", "EMPLEADO", "LIMPIEZA")
@@ -134,24 +104,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /**
-     * Configura y proporciona un bean de tipo {@link AuthenticationManager}.
-     * Este método utiliza {@link AuthenticationConfiguration} para recuperar
-     * y devolver una instancia del manejador de autenticación.
-     *
-     * <p>El objeto {@link AuthenticationManager} es clave para gestionar los flujos
-     * de autenticación en la aplicación.</p>
-     *
-     * @param authConfig Objeto {@link AuthenticationConfiguration} utilizado para crear
-     *                   el manejador de autenticación.
-     *
-     * @return Una instancia de {@link AuthenticationManager} configurada a partir del
-     *         {@link AuthenticationConfiguration} proporcionado.
-     *
-     * @throws Exception Si ocurre algún error durante la creación del manejador de autenticación.
-     *
-     * @Author No se especificó autor.
-     */
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
